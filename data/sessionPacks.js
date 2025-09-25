1d2da98748803bc6bf23cf5966b5c0734ff4ebd6
Víctor Crusat <dev@balancedplus.es>
2025-09-25 21:35:46 +0200

    Eliminados los packs de sesiones, solo se mantiene el individual

--- data/sessionPacks.js
+++ data/sessionPacks.js
@@ -2,23 +2,10 @@
 export const sessionPacks = [
   {
     title: "Sesión Individual",
-    price: "15",
+    price: "20",
     sessionPrice: "",
     type: "individual_session",
     classesCredit: "1"
   },
-  {
-    title: "5 Sesiones",
-    price: "70",
-    sessionPrice: "14€ por sesión",
-    type: "five_sessions",
-    classesCredit: "5"
-  },
-  {
-    title: "10 Sesiones",
-    price: "130",
-    sessionPrice: "13€ por sesión",
-    type: "ten_sessions",
-    classesCredit: "10"
-  }
+
 ];
